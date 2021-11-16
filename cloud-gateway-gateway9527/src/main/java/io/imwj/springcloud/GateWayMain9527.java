6d13680f75bd5524fcea67076bda81ee6f1cf6a4
weixiaojian <2916863213@qq.com>
2021-11-16 18:14:17 +0800

    Gateway项目初始化

---
+++ cloud-gateway-gateway9527/src/main/java/io/imwj/springcloud/GateWayMain9527.java
@@ -0,0 +1,18 @@
+package io.imwj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author langao_q
+ * @since 2021-11-16 18:09
+ */
+@SpringBootApplication
+@EnableEurekaClient
+public class GateWayMain9527 {
+    public static void main(String[] args) {
+        SpringApplication.run( GateWayMain9527.class,args);
+    }
+}
+
