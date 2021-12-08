df801d26bcab221f804eab03634d1250886620f9
weixiaojian <2916863213@qq.com>
2021-12-08 20:16:32 +0800

    nacos

---
+++ cloudalibaba-provider-payment9001/src/main/java/io/imwj/springcloud/PaymentMain9001.java
@@ -0,0 +1,17 @@
+package io.imwj.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * @author langao_q
+ * @since 2021-12-08 16:46
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class PaymentMain9001 {
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentMain9001.class,args);
+    }
+}
